35f87445a04b34f797a7f60864c4a335ec62225e
meyota <meyota08@gmail.com>
2022-09-21 13:47:00 -0700

    tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_node;
 
-	if (parent = NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	right_node = binary_tree_node(parent, value);
