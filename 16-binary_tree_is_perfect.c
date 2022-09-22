15be39adee482875ce49545580978839a4a4b1ce
meyota <meyota08@gmail.com>
2022-09-22 11:27:42 -0700

    tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,6 +13,41 @@ int tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left && tree->right)
 	{
 		left = 1 + tree_is_perfect(tree->left);
-		right = 1 + tree_is_perfect(tree->right)
+		right = 1 + tree_is_perfect(tree->right);
+		if (right == left && right != 0 && left != 0)
+			return (right);
+		return (0);
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (left);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * binary_tree_is_perfect - perfect or not a tree
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int res = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		res = tree_is_perfect(tree);
+		if (res != 0)
+		{
+			return (1);
+		}
+		return (0);
 	}
 }
