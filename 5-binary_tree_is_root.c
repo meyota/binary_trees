8b118d7aad308e8b53d8ef12652cf954d55aaf78
meyota <meyota08@gmail.com>
2022-09-22 05:32:39 -0700

    tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,9 +6,11 @@
  * Return: 1 if node is a root,
  * Otherwise - returns 0
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
 		return (0);
+
 	return(1);
 }
